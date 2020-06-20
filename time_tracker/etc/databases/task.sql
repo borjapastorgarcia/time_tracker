8ad9b1fa09a9c1bab2cb62c92ba42aebb757cd83
borja <borjapg616@gmail.com>
2020-06-20 12:06:09 +0200

    add database structure

--- time_tracker/etc/databases/task.sql
+++ time_tracker/etc/databases/task.sql
@@ -3,3 +3,17 @@ id VARCHAR(36) NOT NULL,
 name VARCHAR(255) DEFAULT NULL
 UNIQUE INDEX idx (id), PRIMARY KEY(id))
 DEFAULT CHARACTER SET utf8 COLLATE `utf8_general_ci` ENGINE = InnoDB;
+
+
+CREATE TABLE time_tracker.task_detail (
+	id varchar(36) NOT NULL,
+	name varchar(255) NOT NULL,
+	status BOOL NOT NULL,
+	started_at DATETIME NOT NULL,
+	stopped_at DATETIME NULL,
+	task varchar(36) NOT NULL,
+	CONSTRAINT task_detail_FK FOREIGN KEY (id) REFERENCES time_tracker.task(id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
