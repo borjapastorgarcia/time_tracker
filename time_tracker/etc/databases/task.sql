3cb8151f95f39a5a9f2215c454a208dfee076dcd
borja <borjapg616@gmail.com>
2020-06-26 17:13:23 +0200

    updated sql

--- time_tracker/etc/databases/task.sql
+++ time_tracker/etc/databases/task.sql
@@ -1,7 +1,6 @@
 CREATE TABLE task (
 id VARCHAR(36) NOT NULL,
-name VARCHAR(255) DEFAULT NULL
-UNIQUE INDEX idx (id), PRIMARY KEY(id))
+name VARCHAR(255) DEFAULT NULL)
 DEFAULT CHARACTER SET utf8 COLLATE `utf8_general_ci` ENGINE = InnoDB;
 
 
@@ -12,9 +11,7 @@ CREATE TABLE time_tracker.task_detail (
 	started_at DATETIME NOT NULL,
 	stopped_at DATETIME NULL,
 	elapsed_time DATETIME NULL,
-	task varchar(36) NOT NULL,
-    UNIQUE INDEX idx (id), PRIMARY KEY(id),
-	CONSTRAINT task_detail_FK FOREIGN KEY (task) REFERENCES time_tracker.task(id)
+	task_id varchar(36) NOT NULL
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8
