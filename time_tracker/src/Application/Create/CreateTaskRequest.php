9ab6b88d97bad37a529defa99c953cddf524bf65
borja <borjapg616@gmail.com>
2020-09-15 23:37:23 +0200

    Refactored Controller and creator

--- time_tracker/src/Application/Create/CreateTaskRequest.php
+++ time_tracker/src/Application/Create/CreateTaskRequest.php
@@ -8,5 +8,16 @@ namespace App\Application\Create;
 
 final class CreateTaskRequest
 {
+    private $name;
+
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
 
 }
--- time_tracker/src/Application/Create/TaskCreator.php
+++ time_tracker/src/Application/Create/TaskCreator.php
@@ -5,25 +5,31 @@ namespace App\Application\Create;
 
 
 use App\Domain\Task;
+use App\Domain\TaskId;
+use App\Domain\TaskName;
 use App\Domain\TaskRepository;
+use App\Shared\Domain\ValueObject\Uuid;
 
 class TaskCreator
 {
     private $repository;
 
-    public function __construct(
-        TaskRepository $repository
-    )
+    public function __construct(TaskRepository $repository)
     {
         $this->repository = $repository;
     }
 
-    public function __invoke(
-        string $name
-    )
+    public function __invoke(CreateTaskRequest $request)
     {
+        $name = new TaskName($request->name());
 
-        $task = Task::create($name);
+        //$id = new TaskId();
+        //$value = TaskId::random();
+
+        $task = new Task(
+            Uuid::random()->value(),
+            $name
+        );
         $this->repository->save($task);
     }
 
